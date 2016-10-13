273a02a1dac56dd85c33c39202ab6e70027df958
Rachelle Faroul <rachelle.faroul@gmail.com>
2016-10-13 16:46:27 -0400

    map rendering

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Map from './components/Map'
 
 class App extends Component {
 	render(){
+
+		const location = {
+			lat: 40.7575285,
+			lng: -73.9884469
+		}
 		return(
 			<div>
 				This is the REACT APP!
-				<div style={{width:300, height:600, background:'red'}}></div>
+				<div style={{width:300, height:600, background:'red'}}>
+					<Map center={location} />
+				</div>
 			</div>
 		)
 	}
---
+++ src/components/Map.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps'
+
+class Map extends Component {
+	render(){
+		const mapContainer = <div style={{height: '100%', width: '100%'}}></div>
+
+		return (
+			<GoogleMapLoader
+		        containerElement = { mapContainer }
+		        googleMapElement = {
+			        <GoogleMap
+			            defaultZoom={18}
+			            defaultCenter={this.props.center}
+			            options={{streetViewControl: false, mapTypeControl: false}}>
+			        </GoogleMap>
+		    } />
+		)
+	}
+}
+
+export default Map
